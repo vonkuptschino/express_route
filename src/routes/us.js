eb60dd8c84affa8c9e889bfdcc08599fab74b8ef
Sophia Kramar <krasofija@yandex.ru>
2018-04-12 18:04:24 +0300

    Update us.js

--- src/routes/us.js
+++ src/routes/us.js
@@ -1,6 +1,8 @@
 const User = require('./../bd')
 
-// curl -s -i -H 'Content-Type: application/json' 'localhost:4321/users' -d '{"login":"student"}'
+// curl -s -i -H 'Content-Type: application/json' 'localhost:4321/users' -d '{"login":"student"}' вывод пароля
+// curl -X PUT -i -H 'Content-Type: application/json' 'localhost:4321/users' -d '{"login":"semiklassnitza", "password":"research"}' добавление юзера
+// curl -X DELETE -i -H 'Content-Type: application/json' 'localhost:4321/users' -d '{"login":"grecha"}' удаление юзера
 
 module.exports = (x) => {
  const rtr = x.Router();
@@ -16,10 +18,21 @@ module.exports = (x) => {
   .post(async r => {
   	const item = await User.findOne({"login": r.body.login});
   	r.res.send(item.login + ': ' + item.password + '\n');
-  });
+  })
+ 
+ .put(async r => {
+    let login = r.body.login;
+    let password = r.body.password;           
+    let myuser = new User({login, password});
+    await myuser.save();
+    r.res.send('added \n');
+  })
 
-  // модуль с добавлением и удалением
-  
+  .delete(async r => {
+    let login = r.body.login;
+    await User.deleteOne({login}); 
+    r.res.send('deleted \n');
+  });
 
  return rtr;
 }
